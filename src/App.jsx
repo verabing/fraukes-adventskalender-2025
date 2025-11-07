6cc1459ba4744bc5b3dd0f257634ce9f335e91ff
verabing <131453885+verabing@users.noreply.github.com>
2025-11-07 11:26:29 +0100

    Create App component for Advent calendar

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-red-600 text-white text-3xl">
+      Fraukes Adventskalender 2025 🎄
+    </main>
+  );
+}
