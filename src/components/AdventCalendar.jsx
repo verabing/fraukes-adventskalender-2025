70a7727cc5a7c66ff3811db2688e77dcfa4045af
verabing <131453885+verabing@users.noreply.github.com>
2025-11-13 19:43:22 +0100

    Update AdventCalendar.jsx

--- src/components/AdventCalendar.jsx
+++ src/components/AdventCalendar.jsx
@@ -145,7 +145,8 @@ export default function AdventCalendar({ year = 2025, monthIndex = 11 }) {
                 key={dayNumber}
                 className="break-inside-avoid mb-4 flex justify-center"
               >
-          <button
+              </div>div>
+                <button
   onClick={() => handleOpenDay(dayNumber, index)}
   className="relative w-full shadow-md hover:shadow-lg transition-all cursor-pointer"
   style={{
