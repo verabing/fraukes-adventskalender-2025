62f9e7ce323c5a4a3c48dc7eae43e15360d22ce8
verabing <131453885+verabing@users.noreply.github.com>
2025-11-23 21:24:08 +0100

    Update tailwind.config.js
    mit wackeln

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wobble: {
+          "0%, 100%": { transform: "rotate(0deg)" },
+          "25%": { transform: "rotate(1.5deg)" },
+          "75%": { transform: "rotate(-1.5deg)" },
+        },
+      },
+      animation: {
+        wobble: "wobble 0.25s ease-in-out",
+      },
+    },
   },
   plugins: [],
 };
+
