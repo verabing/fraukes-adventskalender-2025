89ea9490075541dd515e962d7ac22bab8bba9db1
verabing <131453885+verabing@users.noreply.github.com>
2025-11-24 10:32:06 +0100

    Update tailwind.config.js
    wobble und karrussell funktionen

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,28 @@ export default {
           "25%": { transform: "rotate(1.5deg)" },
           "75%": { transform: "rotate(-1.5deg)" },
         },
+
+        // NEU: Fade + Slide
+        fadeSlideLeft: {
+          "0%":   { opacity: 0, transform: "translateX(-40px)" },
+          "100%": { opacity: 1, transform: "translateX(0)" }
+        },
+        fadeSlideRight: {
+          "0%":   { opacity: 0, transform: "translateX(40px)" },
+          "100%": { opacity: 1, transform: "translateX(0)" }
+        },
       },
+
       animation: {
         wobble: "wobble 0.25s ease-in-out",
+
+        // NEU: Fade + Slide
+        fadeSlideLeft: "fadeSlideLeft 0.45s ease",
+        fadeSlideRight: "fadeSlideRight 0.45s ease",
       },
     },
   },
   plugins: [],
 };
+
 
