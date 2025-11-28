19087743d09286c94ab3f5dc6091f9e54b6e1d63
verabing <131453885+verabing@users.noreply.github.com>
2025-11-28 08:52:44 +0100

    Create de.js

---
+++ lang/de.js
@@ -0,0 +1,54 @@
+const lang_de = {
+  headerTitle: "Fraukes Adventskalender 2025",
+
+  days: {
+    1: {
+      title: "1. Dezember",
+      text: [
+        "Willkommen zu meinem Adventskalender!",
+        "Dieses Jahr habe ich mir besonders viel Mühe gemacht,",
+        "und ihn selber gebaut..."
+      ]
+    },
+
+    2: {
+      title: "2. Dezember",
+      text: [
+        "Text 1 für Bild 1",
+        "Text 2 für Bild 2",
+        "Text 3 für Bild 3"
+      ]
+    },
+
+    3: {
+      title: "3. Dezember",
+      text: [
+        "Text für Bild 1",
+        "Text für Bild 2",
+        "Text für Bild 3"
+      ]
+    },
+
+    4: { title: "4. Dezember", text: [""] },
+    5: { title: "5. Dezember", text: [""] },
+    6: { title: "6. Dezember", text: [""] },
+    7: { title: "7. Dezember", text: [""] },
+    8: { title: "8. Dezember", text: [""] },
+    9: { title: "9. Dezember", text: [""] },
+    10: { title: "10. Dezember", text: [""] },
+    11: { title: "11. Dezember", text: [""] },
+    12: { title: "12. Dezember", text: [""] },
+    13: { title: "13. Dezember", text: [""] },
+    14: { title: "14. Dezember", text: [""] },
+    15: { title: "15. Dezember", text: [""] },
+    16: { title: "16. Dezember", text: [""] },
+    17: { title: "17. Dezember", text: [""] },
+    18: { title: "18. Dezember", text: [""] },
+    19: { title: "19. Dezember", text: [""] },
+    20: { title: "20. Dezember", text: [""] },
+    21: { title: "21. Dezember", text: [""] },
+    22: { title: "22. Dezember", text: [""] },
+    23: { title: "23. Dezember", text: [""] },
+    24: { title: "24. Dezember", text: [""] }
+  }
+};
