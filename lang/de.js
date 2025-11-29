7c92aedcfc72b07edc966c53148677be5ec44672
verabing <131453885+verabing@users.noreply.github.com>
2025-11-29 23:58:19 +0100

    Update de.js

--- lang/de.js
+++ lang/de.js
@@ -2,21 +2,21 @@ const lang_de = {
   headerTitle: "Fraukes Adventskalender 2025",
 
   days: {
-   1: {
+    1: {
       title: "1. Dezember",
       text: [
         "Willkommen bei meinem Kritzelei Adventskalender 2025!",
         "Dieses Jahr habe ich mir besonders viel Mühe für euch gemacht,",
-        "…und ihn von Grund auf selbst gebaut...html, css, java script… und viel ¤#%“#“#¤&%¤¤ !! ",
-	„Zusammen mit meiner KI-Doula chatGPT hab ich’s dann noch rechtzeitig zum 1.12. geschafft, euch diesen Adventskalender fertigzustellen. <br> Viel Vergnügen! “
+        "…und ihn von Grund auf selbst gebaut... HTML, CSS, JavaScript… und viel ¤#%“#“#¤&%¤¤ !! ",
+        "Zusammen mit meiner KI-Doula chatGPT hab ich’s dann noch rechtzeitig zum 1.12. geschafft, euch diesen Adventskalender fertigzustellen. <br> Viel Vergnügen!"
       ]
     },
 
     2: {
       title: "2. Dezember",
       text: [
-        " ",
-        " Viele liebe Menschen haben zu diesem Kalender mit ihren Kritzeln beigetragen.",
+        "",
+        "Viele liebe Menschen haben zu diesem Kalender mit ihren Kritzeln beigetragen.",
         "Danke für diesen Kritzel, Kristoffer."
       ]
     },
@@ -30,94 +30,165 @@ const lang_de = {
       ]
     },
 
-    4: { title: "4. Dezember", text: [
-"Ich werde gefragt: Wie kommst du nur darauf?",
-„Und wenn ich mir die Kritzel anschaue, frage ich mich das oft selbst:“,
-„'Wer kommt den auf so einen Quatsch?'“
-] },
-    5: { title: "5. Dezember", text: [
-"Drachen und Monster sind wiederkehrende Motive",
-„…Monster springen einem förmlich entgegen, aus diesen Kritzeln.“,
-„Danke, Tale, für diesen Monster-Kritzel“
-] },
-    6: { title: "6. Dezember", text: [
-"Ein Wurm mit … Sonnenbrille?",
-„Ich hab auch keine Ahnung! Das schöne an diesen Kritzeleien ist…“,
-„… da sie ja nicht aus meiner Feder stammen, brauch ich sie auch nicht zu erklären.<br> Danke Thomas für diesen Kritzel.“
-] },
-    7: { title: "7. Dezember", text: [
-"'Sie erinnern mich an Piggeldy und Frederick', sagte Katrin,“,
-„die mir diesen Kritzel gemacht hatte.“,
-„'Danke für das Kompliment', antwortete ich und freute mich über diesen Vergleich.“
-] },
-    8: { title: "8. Dezember", text: [
-"Teufel, wer hat sich denn hier versteckt?",
-„Manchmal sind die vermeindlich „einfachen“ Kritzel die hintergründigsten!“,
-„Danke an Kai für diesen Kritzel.“
-] },
-    9: { title: "9. Dezember", text: [
-"Wie heisst es im Sprichwort noch: 'Du sollst nicht den Ast absägen auf dem du sitzt?'",
-„Aber der Kritzel war doch eindeutig: Mann steht auf einem Hai und beugt sich zu ihm herunter...“,
-„Danke an Markus für diesen Kritzel.“
-] },
-    10: { title: "10. Dezember", text: [
-"Und manche Kritzel bleiben auch lange liegen, bevor ich sehe, was darin steckt:“,
-„Bei diesem Kritzel konnte ich lange nichts Richtiges drin erkennen, bis es plötzlich Klick machte“,
-„Danke Bård für diesen Kritzel.“
-] },
-    11: { title: "11. Dezember", text: [
-"Und manche Kritzeleien erzählen richtige Geschichten",
-„… was sich so alles im Gewirr der Kritzelfäden entspinnen kann…“,
-„Eine Nixe, ein Gespenst und ein kleiner Kraken….",
-„und wer entführt hier wen – bzw. wer hat hier vor dem Angst??“,
-„Danke an Emilie für diesen vielschichtigen Kritzel.“
-] },
-    12: { title: "12. Dezember", text: [
-"Und manchmal sind Kritzel einfach auch herrlich geradeaus:",
-"Bei diesem Kritzel bestand einfach nie ein Zweifel, was drin steckt:",
-"Danke Alex für diesen fröhlichen Igel Kritzel."
-] },
-    13: { title: "13. Dezember", text: [
-"Reise-Kritzel mag ich auch wieder gern: Wo geht’s wohl hin, denk ich mir, wenn ich sie…",
-"… in einem Kritzel entdecke. Das Flugzeug-Bullauge zur Welt“,
-"Danke an Thorsten für diesen Reise-Kritzel“
-] },
-    14: { title: "14. Dezember", text: [
-"Herrlich sinnfrei. Was wir hier transportiert? Von wo nach wo? Und wieso? Ich habe mal wieder keine Ahnung. Herrlich.",
-"… aber es steckte eindeutig im Kritzel drin: diese verwirrenden Transportbänder“,
-"Hier mal mit einer Zwischenaufnahme – „,
-"Danke Svein für diesen Kritzel.“
-] },
+    4: {
+      title: "4. Dezember",
+      text: [
+        "Ich werde gefragt: 'Wie kommst du nur darauf?'",
+        "Und wenn ich mir die Kritzel anschaue, frage ich mich das oft selbst:",
+        "'Wer kommt denn auf so einen Quatsch?'"
+      ]
+    },
+
+    5: {
+      title: "5. Dezember",
+      text: [
+        "Drachen und Monster sind wiederkehrende Motive",
+        "…Monster springen einem förmlich entgegen, aus diesen Kritzeln.",
+        "Danke, Tale, für diesen Monster-Kritzel"
+      ]
+    },
+
+    6: {
+      title: "6. Dezember",
+      text: [
+        "Ein Wurm mit … Sonnenbrille?",
+        "Ich hab auch keine Ahnung! Das schöne an diesen Kritzeleien ist…",
+        "… da sie ja nicht aus meiner Feder stammen, brauch ich sie auch nicht zu erklären.<br> Danke Thomas für diesen Kritzel."
+      ]
+    },
+
+    7: {
+      title: "7. Dezember",
+      text: [
+        "'Sie erinnern mich an Piggeldy und Frederick', sagte Katrin,",
+        "die mir diesen Kritzel gemacht hatte.",
+        "'Danke für das Kompliment', antwortete ich und freute mich über diesen Vergleich."
+      ]
+    },
+
+    8: {
+      title: "8. Dezember",
+      text: [
+        "Teufel, wer hat sich denn hier versteckt?",
+        "Manchmal sind die vermeintlich 'einfachen' Kritzel die hintergründigsten!",
+        "Danke an Kai für diesen Kritzel."
+      ]
+    },
+
+    9: {
+      title: "9. Dezember",
+      text: [
+        "Wie heisst es im Sprichwort noch: 'Du sollst nicht den Ast absägen, auf dem du sitzt?'",
+        "Aber der Kritzel war doch eindeutig: Mann steht auf Hai und beugt sich zu ihm herunter",
+        "Danke an Markus für diesen Kritzel."
+      ]
+    },
+
+    10: {
+      title: "10. Dezember",
+      text: [
+        "Und manche Kritzel bleiben auch lange liegen, bevor ich sehe, was darin steckt:",
+        "Bei diesem Kritzel konnte ich lange nichts Richtiges drin erkennen, bis es plötzlich Klick machte",
+        "Danke Bård für diesen Kritzel."
+      ]
+    },
+
+    11: {
+      title: "11. Dezember",
+      text: [
+        "Und manche Kritzeleien erzählen richtige Geschichten",
+        "… was sich so alles im Gewirr der Kritzelfäden entspinnen kann…",
+        "Eine Nixe, ein Gespenst und ein kleiner Kraken…",
+        "und wer entführt hier wen – bzw. wer hat hier vor wem Angst?",
+        "Danke an Emilie für diesen vielschichtigen Kritzel."
+      ]
+    },
+
+    12: {
+      title: "12. Dezember",
+      text: [
+        "Und manchmal sind Kritzel einfach auch herrlich geradeaus:",
+        "Bei diesem Kritzel bestand einfach nie ein Zweifel, was drin steckt:",
+        "Danke Alex für diesen fröhlichen Igel-Kritzel."
+      ]
+    },
+
+    13: {
+      title: "13. Dezember",
+      text: [
+        "Reise-Kritzel mag ich auch wieder gern: Wo geht’s wohl hin, denk ich mir, wenn ich sie…",
+        "… in einem Kritzel entdecke. Das Flugzeug-Bullauge zur Welt",
+        "Danke an Thorsten für diesen Reise-Kritzel"
+      ]
+    },
+
+    14: {
+      title: "14. Dezember",
+      text: [
+        "Herrlich sinnfrei. Was wird hier transportiert? Von wo nach wo? Und wieso? Ich habe mal wieder keine Ahnung. Herrlich.",
+        "… aber es steckte eindeutig im Kritzel drin: diese verwirrenden Transportbänder",
+        "Hier mal mit einer Zwischenaufnahme – ",
+        "Danke Svein für diesen Kritzel."
+      ]
+    },
+
     15: { title: "15. Dezember", text: [""] },
-    16: { title: "16. Dezember", text: [
-"Und manchmal küsst einen die Muse auf ganz naiv romantische Weise,",
-"Weil es einfach ganz deutlich so im Kritzel steckt.“,
-"Danke Patrick für diesen Miro Kritzel“
-] },
-    17: { title: "17. Dezember", text: [
-"Komische Vögel sind natürlich auch immer wieder Klassiker unter den Kritzeln",
-"Ihr seht ihn auch ganz deutlich, oder?“,
-"Für diesen Kritzel darf ich mir mal selbst danken.“
-] },
+
+    16: {
+      title: "16. Dezember",
+      text: [
+        "Und manchmal küsst einen die Muse auf ganz naiv romantische Weise,",
+        "Weil es einfach ganz deutlich so im Kritzel steckt.",
+        "Danke Patrick für diesen Miro-Kritzel"
+      ]
+    },
+
+    17: {
+      title: "17. Dezember",
+      text: [
+        "Komische Vögel sind natürlich auch immer wieder Klassiker unter den Kritzeln",
+        "Ihr seht ihn auch ganz deutlich, oder?",
+        "Für diesen Kritzel darf ich mir mal selbst danken."
+      ]
+    },
+
     18: { title: "18. Dezember", text: [""] },
     19: { title: "19. Dezember", text: [""] },
-    20: { title: "20. Dezember", text: [
-"Wer kommt denn da auf seinem Schlitten angeritten?",
-"Manchmal muss ich die Kritzel auch auf den Kopf stellen, um zu sehen, was drinsteckt.“,
-"Danke Yngve für diesen Kritzel.“
-] },
-    21: { title: "21. Dezember", text: [
-"Diese Kritzelei ergab sich aus den Kritzeln von zwei lieben Kolleginnen – Teamarbeit ist unser Motto!",
-"Danke an Marit und“,
-"Danke an Marianne, für die gute Zusammenarbeit.“
-] },
-    22: { title: "22. Dezember", text: [
-"Kamelkarawane… es trägt bestimmt Gold, Weihrauch und Myrrhe für die drei Könige",
-"… ich sehe jetzt, dass genausogut eine Gespensterprozession daraus hätte werden konnen…“,
-„ “
-] },
-    23: { title: "23. Dezember", text: [""] },
-    24: { title: "24. Dezember", text: [
-"Frohe Weihnachten!"
-] }
 
+    20: {
+      title: "20. Dezember",
+      text: [
+        "Wer kommt denn da auf seinem Schlitten angeritten?",
+        "Manchmal muss ich die Kritzel auch auf den Kopf stellen, um zu sehen, was drinsteckt.",
+        "Danke Yngve für diesen Kritzel."
+      ]
+    },
+
+    21: {
+      title: "21. Dezember",
+      text: [
+        "Diese Kritzelei ergab sich aus den Kritzeln von zwei lieben Kolleginnen – Teamarbeit ist unser Motto!",
+        "Danke an Marit und",
+        "Danke an Marianne, für die gute Zusammenarbeit."
+      ]
+    },
+
+    22: {
+      title: "22. Dezember",
+      text: [
+        "Kamelkarawane… es trägt bestimmt Gold, Weihrauch und Myrrhe für die drei Könige",
+        "… ich sehe jetzt, dass genauso gut eine Gespensterprozession daraus hätte werden können…",
+        ""
+      ]
+    },
+
+    23: { title: "23. Dezember", text: [""] },
+
+    24: {
+      title: "24. Dezember",
+      text: [
+        "Frohe Weihnachten!"
+      ]
+    }
+  }
+};
