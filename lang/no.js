1e969d1747c7a0eadbec1163f6c20ce5347f5b8c
verabing <131453885+verabing@users.noreply.github.com>
2025-11-29 21:03:40 +0100

    Create no.js

---
+++ lang/no.js
@@ -0,0 +1,54 @@
+const lang_de = {
+  headerTitle: "Fraukes Adventskalender 2025",
+
+  days: {
+    1: {
+      title: "1. Desember",
+      text: [
+        "Velkommen til min krusseduller julekalender!",
+        "I år har jeg gjort meg ekstra flid med kalenderen",
+        "da jeg har bygget den selv - sammen med min KI-doula chatgpt."
+      ]
+    },
+
+    2: {
+      title: "2. Desember",
+      text: [
+        "Text 1 für Bild 1",
+        "Text 2 für Bild 2",
+        "Text 3 für Bild 3"
+      ]
+    },
+
+    3: {
+      title: "3. Dezember",
+      text: [
+        "Text für Bild 1",
+        "Text für Bild 2",
+        "Text für Bild 3"
+      ]
+    },
+
+    4: { title: "4. Desember", text: [""] },
+    5: { title: "5. Desember", text: [""] },
+    6: { title: "6. Desember", text: [""] },
+    7: { title: "7. Dezember", text: [""] },
+    8: { title: "8. Dezember", text: [""] },
+    9: { title: "9. Dezember", text: [""] },
+    10: { title: "10. Dezember", text: [""] },
+    11: { title: "11. Dezember", text: [""] },
+    12: { title: "12. Dezember", text: [""] },
+    13: { title: "13. Dezember", text: [""] },
+    14: { title: "14. Dezember", text: [""] },
+    15: { title: "15. Dezember", text: [""] },
+    16: { title: "16. Dezember", text: [""] },
+    17: { title: "17. Dezember", text: [""] },
+    18: { title: "18. Dezember", text: [""] },
+    19: { title: "19. Dezember", text: [""] },
+    20: { title: "20. Dezember", text: [""] },
+    21: { title: "21. Dezember", text: [""] },
+    22: { title: "22. Dezember", text: [""] },
+    23: { title: "23. Dezember", text: [""] },
+    24: { title: "24. Dezember", text: [""] }
+  }
+};
